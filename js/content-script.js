0dce0b57e5915542acaaab06d2a3053c00848e68
J-W-Chan <j_w_chan@outlook.com>
2020-09-14 19:21:41 +0800

    实现存储账号逻辑

--- js/content-script.js
+++ js/content-script.js
@@ -1,11 +1,20 @@
-window.onload = function(){
-    document.getElementById("signInSubmit").addEventListener("click",function(){
-        var site = "amazon.com";
-        var username = document.getElementById("ap_email").innerText;
-        var password = document.getElementById("ap_password").innerText;
-    
-        console.log("username: "+username);
-        console.log("password: "+password);
-    },false);
+window.onload = function () {
+    //console.log("ssssss");
+    var signInBtn = document.getElementById("signInSubmit");
+    if (signInBtn) {
+        signInBtn.addEventListener("click", function () {
+            //var site = "amazon.com";
+            var username = document.getElementById("ap_email").value;
+            var password = document.getElementById("ap_password").value;
 
-}
+            var storage = window.localStorage;
+
+            storage.setItem("username", username);
+            storage.setItem("password", password);
+
+            console.log("username: " + username);
+            console.log("password: " + password);
+        }, false);
+    }
+};
+
